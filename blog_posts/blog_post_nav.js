f129c109a6a8db821774a4c1b893b06520051dd9
mr-y <martin@ryberg.org>
2020-02-17 15:32:55 +0100

    Testing styling of nav

--- blog_posts/blog_post_nav.js
+++ blog_posts/blog_post_nav.js
@@ -1,18 +1,18 @@
 var make_links = function () {
     console.log(location.pathname);
-    //var posts = require('../database.json');
-    document.getElementById("links").innerHTML = '<a href="../index.html">Home</a>';
-    //console.log(JSON.stringify(posts));
-    //console.log(posts.entries.length);
+    var links = '';
+    var home_link = '<div class="nav-home"><a href="../index.html">Home</a></div>';
     for (var i=0; i<posts.entries.length; ++i) {
 	console.log(posts.entries[i].address);
         if (location.pathname === posts.entries[i].address) {
-            if (i > 0 && posts.entries[i-1].address) document.getElementById("links").innerHTML += ' | <a href="' + posts.entries[i-1].address + '">prev</a>';
-            if (i+1 < posts.length && posts.entries[i+1].address) document.getElementById("links").innerHTML += ' | <a href="' + posts.entries[i+1].address + '">next</a>';
+            if (i > 0 && posts.entries[i-1].address) links = '<a href="' + posts.entries[i-1].address + '">prev</a>' + home_link;
+            if (i+1 < posts.entries.length && posts.entries[i+1].address) links += ' <div class="nav-right"><a href="' + posts.entries[i+1].address + '">next</a></div>';
             break;
         }
     }
-    document.getElementById("links").innerHTML += '<hr>';
+    if (!links) links = home_link;
+    links += '<br><hr>';
+    document.getElementById("links").innerHTML = links;
 }
 document.addEventListener("load",make_links());
 
