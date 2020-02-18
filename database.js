e6fad79c7aadf80ab0cf6d22f9858e927a50c3c8
mr-y <martin@ryberg.org>
2020-02-18 08:27:24 +0100

    Including figures hosted on flickr

--- database.js
+++ database.js
@@ -3,16 +3,8 @@ var posts = {
 	{
 	"address":"/blog/blog_posts/classifications.html",
 	"title":"A little bit about classifications",
-	"summary":"A short rant on classifications in biology"
-	},
-	{
-	"address":"/blog/blog_posts/test_post1.html",
-	"title":"This is just a test",
-	"Summary":"A post for trying out the concept."
-	},
-	{
-        "address":"/blog/blog_posts/nonono.html",
-        "title":"Do not exist"
-        },
+	"summary":"A short rant on classifications in biology",
+	"date":"2020-02-18"
+	}
     ]
 }
